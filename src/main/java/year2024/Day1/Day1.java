c0e5fa7de8bbf13e769fbffa52d38b3d46a5e3a8
haakensonb <untamedhawk44@gmail.com>
2025-06-21 16:24:38 -0700

    Remove import

--- src/main/java/year2024/Day1/Day1.java
+++ src/main/java/year2024/Day1/Day1.java
@@ -2,8 +2,6 @@ package year2024.Day1;
 
 import common.Solution;
 
-import java.util.stream.IntStream;
-
 public class Day1  implements Solution {
     @Override
     public String solvePart1(String input) {
